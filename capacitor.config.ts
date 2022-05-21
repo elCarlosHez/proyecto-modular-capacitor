d08d4b9e78652934050ff404af8d8860708af933
Carlos Arellano <luis.harellano@alumnos.udg.mx>
2022-05-21 12:21:30 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Ingenia tus Finanzas',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
